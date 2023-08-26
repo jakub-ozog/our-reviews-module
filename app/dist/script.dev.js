de81fc3b99d6386c3de2ec2867d4b8ac8590a342
jacobharraldson <jharraldson@gmail.com>
2023-08-27 00:58:08 +0200

    js logic for slide updated/style for bg and card window udpated

--- app/dist/script.dev.js
+++ app/dist/script.dev.js
@@ -1,36 +1,62 @@
 "use strict";
 
-var devDetails = document.querySelectorAll('.card__devDetails');
-console.log(devDetails); // devDetails.forEach(item => {
-//     item.addEventListener('click', () => {
-//         devDetails.forEach(d => {
-//             d.classList.toggle('dNone');
-//         })
-//         item.classList.toggle('dBlock');
-//     });
-// });
-// const previewReview = document.querySelector('.card__preview')
-// previewReview.addEventListener('click', () => {
-//     console.log('test')
-// })
-// const nextReview = document.querySelector('.card__next')
-// devDetails.forEach(item => {
-//     item.addEventListener('click', () => {
-//         console.log(item)
-//     })
-//     nextReview.addEventListener('click', () => {
-//         console.log('test2')
-//     })
-// })
-
-var reviews = [{
+var slider = document.querySelector('.card');
+var prevButton = document.querySelector('.card__preview');
+var nextButton = document.querySelector('.card__next');
+var slides = [{
   id: 1,
-  name: 'susan smith',
-  job: 'web developer',
-  text: 'Im meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast eveyrdau carry'
+  avatar: 'assets/images/person-1.jpg',
+  title: 'Susan Smith',
+  job: 'Web Developer',
+  description: 'Im meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast eveyrdau carry'
 }, {
   id: 2,
-  name: 'anna johnson',
-  job: 'web developer',
-  text: 'Im meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast eveyrdau carry'
+  avatar: 'assets/images/person-2.jpg',
+  title: 'Anna Johnson',
+  job: 'Product Owner',
+  description: 'Ducimus consectetur sint perspiciatis quae ex Suscipit inventore error dignissimos maiores sapiente reprehenderit a distinctio architecto, maxime mollitia veritatis provident asperiores reiciendis sequi?'
+}, {
+  id: 3,
+  avatar: 'assets/images/person-3.jpg',
+  title: 'Bill Anderson',
+  job: 'UX Designer',
+  description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam, fugiat molestiae labore laborum praesentium quae adipisci reiciendis soluta ex modi. Ducimus obcaecati quos quaerat!'
 }];
+var currentIndex = 0;
+
+function renderSlide(index) {
+  var slide = slides[index];
+  var slideHTML = "\n    <div class=\"card__devDetails\">\n    <div class=\"card__devThumb\">\n      <img src=\"".concat(slide.avatar, "\" alt=\"icon-user\">\n    </div>\n    <h2 class=\"card__devTitle\">").concat(slide.title, "</h2>\n    <h3 class=\"card__devSubtitle\">").concat(slide.job, "</h3>\n    <p class=\"card__devDesc\">").concat(slide.description, "</p>\n  </div>\n    ");
+  slider.innerHTML = slideHTML;
+}
+
+function slide(direction) {
+  currentIndex += direction;
+
+  if (currentIndex < 0) {
+    currentIndex = slides.length - 1;
+  } else if (currentIndex >= slides.length) {
+    currentIndex = 0;
+  }
+
+  renderSlide(currentIndex);
+}
+
+prevButton.addEventListener('click', function () {
+  slide(-1);
+});
+nextButton.addEventListener('click', function () {
+  slide(1);
+});
+renderSlide(currentIndex); //Randomize slides
+
+var getRandomNumber = function getRandomNumber() {
+  return Math.floor(Math.random() * 3) + 1;
+};
+
+var randomNumbers = Array.from({
+  length: 10
+}, getRandomNumber);
+randomNumbers.forEach(function (item) {
+  console.log(item);
+});
--- app/script.js
+++ app/script.js
@@ -1,54 +1,80 @@
-const devDetails = document.querySelectorAll('.card__devDetails');
-console.log(devDetails);
-
-// devDetails.forEach(item => {
-//     item.addEventListener('click', () => {
-
-//         devDetails.forEach(d => {
-//             d.classList.toggle('dNone');
-//         })
-
-//         item.classList.toggle('dBlock');
-//     });
-// });
-
-// const previewReview = document.querySelector('.card__preview')
-
-// previewReview.addEventListener('click', () => {
-//     console.log('test')
-// })
-
-// const nextReview = document.querySelector('.card__next')
+const slider = document.querySelector('.card');
+const prevButton = document.querySelector('.card__preview');
+const nextButton = document.querySelector('.card__next');
 
 
-
-
-
-
-// devDetails.forEach(item => {
-
-//     item.addEventListener('click', () => {
-//         console.log(item)
-//     })
-
-//     nextReview.addEventListener('click', () => {
-//         console.log('test2')
-
-
-//     })
-// })
-
-const reviews = [
-    {
+const slides = [{
         id: 1,
-        name: 'susan smith',
-        job: 'web developer',
-        text: 'Im meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast eveyrdau carry',
+        avatar: 'assets/images/person-1.jpg',
+        title: 'Susan Smith',
+        job: 'Web Developer',
+        description: 'Im meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast eveyrdau carry',
     },
     {
         id: 2,
-        name: 'anna johnson',
-        job: 'web developer',
-        text: 'Im meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast eveyrdau carry',
+        avatar: 'assets/images/person-2.jpg',
+        title: 'Anna Johnson',
+        job: 'Product Owner',
+        description: 'Ducimus consectetur sint perspiciatis quae ex Suscipit inventore error dignissimos maiores sapiente reprehenderit a distinctio architecto, maxime mollitia veritatis provident asperiores reiciendis sequi?'
+    },
+    {
+        id: 3,
+        avatar: 'assets/images/person-3.jpg',
+        title: 'Bill Anderson',
+        job: 'UX Designer',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam, fugiat molestiae labore laborum praesentium quae adipisci reiciendis soluta ex modi. Ducimus obcaecati quos quaerat!',
+    },
+];
+
+let currentIndex = 0;
+
+function renderSlide(index) {
+    const slide = slides[index];
+
+    const slideHTML = `
+    <div class="card__devDetails">
+    <div class="card__devThumb">
+      <img src="${slide.avatar}" alt="icon-user">
+    </div>
+    <h2 class="card__devTitle">${slide.title}</h2>
+    <h3 class="card__devSubtitle">${slide.job}</h3>
+    <p class="card__devDesc">${slide.description}</p>
+  </div>
+    `
+
+    slider.innerHTML = slideHTML;
+}
+
+function slide(direction) {
+    currentIndex += direction
+
+    if (currentIndex < 0) {
+        currentIndex = slides.length - 1;
+    } else if (currentIndex >= slides.length) {
+        currentIndex = 0
     }
-]
+
+    renderSlide(currentIndex)
+}
+
+
+prevButton.addEventListener('click', () => {
+    slide(-1);
+});
+
+nextButton.addEventListener('click', () => {
+    slide(1)
+})
+
+renderSlide(currentIndex);
+
+
+//Randomize slides
+
+const getRandomNumber = () => Math.floor(Math.random() * 3) + 1;
+
+const randomNumbers = Array.from({length: 10 }, getRandomNumber);
+
+randomNumbers.forEach(item => {
+    console.log(item);
+})
